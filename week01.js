20b614c387855ec4d20d19ebcdc20a018a2e902b
yujin an <130891452+ynjiuau@users.noreply.github.com>
2023-06-15 22:38:12 +0900

    2번까지 완료

--- week01.js
+++ week01.js
@@ -1,32 +1,41 @@
 //1. 가장 큰 값 구하기
 
-function maxValue(arr) {
-    const max = Math.max (...arr);
+// function maxValue(arr) {
+//     const max = Math.max (...arr);
 
-    const sortArr = arr.sort((a, b) => b - a);
-    //오름차순 내림차순 정렬 (딥다이브 JS 531p)
+//     const sortArr = arr.sort((a, b) => b - a);
+//     //오름차순 내림차순 정렬 (딥다이브 JS 531p)
 
-    const result = {
-        max: max,
-        sortArr: sortArr
+//     const result = {
+//         max: max,
+//         sortArr: sortArr
+//     };
+
+//     return result;
+// }
+
+// const numbers = [1, 2, 3, 4, 5];
+// const result = maxValue(numbers);
+// console.log(result);
+
+
+//2. 편의점 물건 확인하기
+    const stock = {
+        '야채곱창' : 5,
+        '바나나우유' : 10,
+        '삼각김밥' : 15,
+        '도시락' : 10,
+        '샌드위치' : 10
     };
 
-    return result;
-}
+    function stockCheck (item, quanity) {
+        if (stock[item]=== quanity){
+            console.log(`${item}의 갯수가 전산표와 일치합니다`);
+        } else {
+            console.log('전산표와 일치하지 않습니다.');
+        }
+    }
 
-const numbers = [1, 2, 3, 4, 5];
-const result = maxValue(numbers);
-console.log(result);
-
-
-
-
-
-
-// if(
-
-// ){
-
-// } else {
-//     console.log('전산표와 일치하지 않습니다.')
-// }
+    stockCheck('야채곱창', 3); //전산표와 일치하지 않습니다. 출력
+    stockCheck('삼각김밥', 15) //삼각김밥의 갯수가 전산표와 일치합니다 출력
+    
