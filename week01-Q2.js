08e657f37e3224d0839b38ce5976c5230d54ca61
yujin an <130891452+ynjiuau@users.noreply.github.com>
2023-06-16 20:00:17 +0900

    2번 완료

--- week01-Q2.js
+++ week01-Q2.js
@@ -29,15 +29,20 @@ console.log(result);
     };
 
     function stockCheck (item, quanity) {
-        if (stock[item]=== quanity){
-            console.log(`${item}의 갯수가 전산표와 일치합니다`);
-        } else {
-            console.log('전산표와 일치하지 않습니다.');
+        if (item in stock) {
+            if (stock[item]=== quanity){
+                console.log(`${item}의 갯수가 전산표와 일치합니다`);
+            } else {
+                console.log('전산표와 일치하지 않습니다.');
+            }
+        }else {
+            console.log(`${item}` + ' : 항목없음')
         }
     }
 
     stockCheck('야채곱창', 3); //전산표와 일치하지 않습니다. 출력
     stockCheck('삼각김밥', 15) //삼각김밥의 갯수가 전산표와 일치합니다 출력
+    stockCheck('사과', 2)
 
-    //조건 세가지를 달성하는 건 실패함... 어떻게 해야할지 좀 더 고민을...
+   
     
