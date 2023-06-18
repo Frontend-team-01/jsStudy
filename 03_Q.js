de9315281f6739dbfc89685c0ad4387714268368
newsks <sweetysks23@gmail.com>
2023-06-18 20:47:49 +0900

    3번 문제풀이수정

--- 03_Q.js
+++ 03_Q.js
@@ -8,14 +8,14 @@ console.log("결과값이 없습니다")
 
 const arr = [1, 2, 3, 4, 5]
 const newArr = arr.map((item, index, arr) => item + 10);
-console.log(newArr)
+console.log(newArr)   //[ 11, 12, 13, 14, 15 ]
 
 const filterArr = newArr.filter((item) => item >=0);
-console.log(filterArr)
+console.log(filterArr) //[ 11, 12, 13, 14, 15 ]
 
 const filterArr2 = newArr.filter((item2) => item2 < 0);
-console.log(filterArr2,"결과값이 없습니다")
+console.log(filterArr2,"결과값이 없습니다")  //[] 결과값이 없습니다
 
 const spArr = filterArr.splice(1)
-console.log(spArr, "(값있음)")
+console.log(spArr, "(값있음)") // [ 12, 13, 14, 15 ] (값있음)
 
