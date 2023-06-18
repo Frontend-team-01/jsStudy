6307dbd85ac2ff9e61a06793de2034d40b347df8
newsks <sweetysks23@gmail.com>
2023-06-18 10:17:21 +0900

    2번문제 다시 정리

--- 02_Q.js
+++ 02_Q.js
@@ -1,21 +1,27 @@
-let shops = {
-  'VegetablesBeef': 5,
-  '🍌': 10,
-  '🍙': 15,
-  '🍱': 10,
-  '🥪': 10,
-}; 
+/* 
+---성공---
+console.log("전산표와 일치합니다.")
 
-function calc(name, count) {
-  if (name in shops) {
-    if (count === shops[name]) {
-      console.log(`"전산표와 일치합니다."`);
-    } else {
-      console.log(`"전산표와 일치하지 않습니다."`);
-    }
+---예외(실패)---
+console.log("전산표와 일치하지 않습니다.")
+console.log("품목이 없습니다.")
+*/
+
+function 편의점(product,count) {
+
+  const menus={야채곱창:5, 바나나우유:10, 삼각김밥:15, 도시락:10, 샌드위치:10};
+  
+  if (product in menus) { 
+    if (count === menus[product]) {
+        console.log(`"전산표와 일치합니다."`);
+      } else {
+        console.log(`"전산표와 일치하지 않습니다."`);
+      }     
   } else {
-    console.log(`"${name}" (품목 없음)`);
+    console.log(`"${product}" (품목 없음)`);
   }
 }
 
-calc("🍙", 8);
+//편의점('콜라',10)
+//편의점('바나나우유',10)
+편의점('바나나우유',7)
