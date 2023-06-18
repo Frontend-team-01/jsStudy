e6c28e332cad91a19ac604ccf7e9ab08764ac2e4
newsks <sweetysks23@gmail.com>
2023-06-18 21:00:09 +0900

    4번 문제풀이 수정

--- 04_Q.js
+++ 04_Q.js
@@ -1,17 +1,9 @@
-const arr = [1, 2, 3, 4, 5];
-const even = [];
-const odd = [];
+/* 
+---성공---
+[2,3] [짝수의 개수, 홀수의 개수]
+*/
 
-function solution(counter) {
-  for (let i = 0; i < counter.length; i++) {
-    if (counter[i] % 2 === 0) {
-      even.push(counter[i]);
-    } else {
-      odd.push(counter[i]);
-    }
-  }
-  return `[${even.length}, ${odd.length}]`;
-}
-
-const a = solution(arr);
-console.log(a);
+const arr = [1, 2, 3, 4, 5]
+const _evenArr = arr.filter((item) => item%2===0)
+const _oddArr = arr.filter((item2) => item2%2!=0)
+console.log(_evenArr.length, _oddArr.length,"<- 짝수의 갯수,홀수의 갯수")
