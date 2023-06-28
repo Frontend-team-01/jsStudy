1ab7b19cab025bf7dcbffdcf95b27d3729c6b941
newsks <sweetysks23@gmail.com>
2023-06-28 09:06:07 +0900

    자바스크립트2번수정

--- JS_q2.js
+++ JS_q2.js
@@ -20,8 +20,11 @@ const division = (arr, n) => {
     }
   
     return newArray;
-  }
-  const arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
-  const result = division(arr, 5);
-  console.log(result)
-  //
+}
+
+const myArray = Array(80)
+  .fill()
+  .map((_, i) => i + 1); 
+
+const newArray = division(myArray, 5);
+console.log(newArray)
