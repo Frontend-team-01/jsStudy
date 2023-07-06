dbeef5f8fac64eb1353ce00086b5fe9ec444c6db
Frontend <129296269+newsks@users.noreply.github.com>
2023-07-06 23:37:40 +0900

    Revert "Sookyeong"

--- 1주차/04_Q.js
+++
@@ -1,9 +0,0 @@
-/* 
----성공---
-[2,3] [짝수의 개수, 홀수의 개수]
-*/
-
-const arr = [1, 2, 3, 4, 5]
-const _evenArr = arr.filter((item) => item%2===0)
-const _oddArr = arr.filter((item2) => item2%2!=0)
-console.log(_evenArr.length, _oddArr.length,"<- 짝수의 갯수,홀수의 갯수")
