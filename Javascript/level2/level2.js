3013af9dc3482b08dae7076e5a223f35b4e7a169
newsks <sweetysks23@gmail.com>
2023-07-10 22:52:53 +0900

    자바스크립트 level2문제완료

--- Javascript/level2/level2.js
+++ Javascript/level2/level2.js
@@ -1,5 +1,5 @@
 import { RESERVATION_LIST } from './reservation .js';
-//console.log(RESERVATION_LIST);
+// console.log(RESERVATION_LIST[1].name);
 
 /* 
 예약 고객확인하기
@@ -8,27 +8,33 @@ import { RESERVATION_LIST } from './reservation .js';
 
 */
 
-const $confirmBtn =document.querySelector('.confirmBtn')
-const $name = document.querySelector('.user-name')
-const $phone = document.querySelector('user-phone')
-const content = $phone
+const form = document.getElementsByTagName("form")[0];
 
-$confirmBtn.addEventListener('click',()=>{
-    if(!$name.value.trim()||!$phone.value.trim()){ 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+const $userName = document.getElementById("user-name");
+const $userPhone = document.getElementById("user-phone");
+const $eventBtn = document.querySelector("button");
+  
+  $eventBtn.addEventListener("click", view);
+  
+  
+  function view() {
+    if(!$userName.value.trim()||!$userPhone.value.trim()){ 
         return alert('내용을 입력해주세요')
-    } else if($name.value == RESERVATION_LIST.$name && $phone.value == RESERVATION_LIST.$phone){
-        
-        const myList = document.getElementById("reservation-number")
-        const newList = document.createElement('p')
-        newText.innerText = content
-        myList.appendChild(newText)
-
-        myList.append(newList)
-    } else {
-        alert("정보와 다릅니다")
     }
+    
+    let find = RESERVATION_LIST.find((user) => 
+    user["name"] === $userName.value && user["phone"] === $userPhone.value);
 
-    document.querySelector('.user-name').value =''
-    document.querySelector('.user-phone').value =''
-})
-
+    if (find) {
+      let num = find["number"];
+      let resultHTML = "";
+      resultHTML += `<div>${num}</div>`;
+      document.getElementById("reservation-number").innerHTML = resultHTML;
+    } else {
+      return alert("일치하는 고객이 없습니다.");
+    }
+  }
