4c1122a8503b91d0beb902932d600e009bee27ea
kiminn <808010x@gmail.com>
2023-07-11 17:43:47 +0900

    '중복값 제거완' -> '값이 한 번에 두개씩 추가되는 오류 발생' '해결요망 ㅠ'

--- 2주차플로우/js_심화/level3/level3.js
+++ 2주차플로우/js_심화/level3/level3.js
@@ -7,23 +7,47 @@ form.addEventListener('submit', (e) => {
     event.preventDefault();
 });
 
+const addItem = document.querySelector('button');
+addItem.addEventListener('click', append);
 
-let inputName;
-let inputWeight;
-let showTbl;
+let inputName = document.getElementById('input-name');
+let inputWeight = document.getElementById('input-weight');
+let showTbl = document.getElementById('show-tbl');
+const nameList = [];
+const weightList = [];
 
 // 추가
-function addItem() {
+function append() {
     console.log('추가');
-    inputName = document.getElementById('input-name').value;
-    inputWeight = document.getElementById('input-weight').value;
-    showTbl = document.getElementById('show-tbl');
+    let obj = {
+        name: inputName.value,
+        id: randomID(),
+    };
+    // push
+    nameList.push(obj['name']);
+    weightList.push(inputWeight.value);
+    // result 객체를 생성
+    const result = {};
+    // itemList 배열을 순회하며 item.value를 key, item.value의 갯수를 value로 가지는 객체를 생성
+    // 중복 값 https://hianna.tistory.com/459
+    nameList.forEach((x) => {
+        result[x] = (result[x] || 0) + 1;
+    });
+    console.log(inputName.value);
+    console.log(inputWeight.value);
+    console.log(result);
 
-    console.log(inputName);
-    console.log(inputWeight);
+    // item.value의 value가 1이 아닐경우(2이상)
+    if (!(result[inputName.value] === 1)) {
+        // 중복값 발생시 itemList에 마지막 인덱스(중복값)을 제거
+        nameList.pop();
+        weightList.pop();
+        // 중복값 발생시 요소를 추가하지 않고 alert()를 return
+        return alert('이미 존재하는 재료입니다.');
+    }
 
-    const content1 = inputName;
-    const content2 = inputWeight;
+    const content1 = inputName.value;
+    const content2 = inputWeight.value;
 
     // https://zetawiki.com/wiki/자바스크립트_HTML_테이블_행_추가/삭제
     let newRow = showTbl.insertRow(showTbl.rows.length); // 하단에 추가
@@ -35,33 +59,26 @@ function addItem() {
     cell3.innerHTML = `<button onclick="deleteRow()">삭제</button>`;
 }
 
+// 이부분 잘모르겠음 (????????)
+function randomID() {
+    return Math.random().toString(36).substr(2, 16);
+}
+
 // 삭제
 function deleteRow() {
-    console.log('삭제')
+    console.log('삭제');
     if (showTbl.rows.length < 1) return;
     showTbl.deleteRow(showTbl.rows.length - 1); // 하단부터 삭제
 }
 
 // 제출 {key : value} 가져오기
-const itemList = [];
-let submitList;
-
-// 중복 값 https://hianna.tistory.com/459
-
 function submit() {
-    console.log('제출')
+    console.log('제출');
     let submitList = document.querySelector('#ingredient-list');
-
     const ingredient = document.getElementById('input-name').value;
     const weight = document.getElementById('input-weight').value;
-
     const newList = document.createElement('li');
     console.log(newList);
-
-    // 콘솔창용
-    a['재료'] = ingredient;
-    a['무게'] = weight;
-    console.log(a);
 
     const spanElement = document.createElement('span');
     const span2Element = document.createElement('span');
@@ -74,6 +91,3 @@ function submit() {
 
     submitList.append(newList);
 }
-
-
-
